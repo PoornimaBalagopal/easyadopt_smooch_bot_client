9c2ebcf9e90178c484997767d98b9ff55e97cf3f
PoornimaBalagopal <poornima.balagopal@gmail.com>
2018-12-18 20:41:52 +0530

    modified to fix the tag display in the preview

--- scripts/custom_tag_loader.js
+++ scripts/custom_tag_loader.js
@@ -18,7 +18,7 @@ const tags = [
 
 function deleteTags(message) {
     for (let i in tags) message = message.replace(new RegExp(tags[i].escaped, "g"), "");
-    return message.replace(/&lt;.?ea_\w+>?/g, "");
+    return message.replace(/&lt;?.ea-\w+(?:&gt, "");
 }
 
 function unescapeMessage(message) {
