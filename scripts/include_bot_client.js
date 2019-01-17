b2c9181b16995d4eba800aa3135beae8461580da
Poornima Balagopal <poornima.balagopal@gmail.com>
2019-01-17 14:08:14 +0530

    Update include_bot_client.js

--- scripts/include_bot_client.js
+++ scripts/include_bot_client.js
@@ -212,10 +212,11 @@ function getJWT() {
 	if(xhttp.readyState === 4 && xhttp.status === 200) {
 		
 		access_token=JSON.parse(xhttp.responseText).access_token;
-		console.log("------JSON DATA------");
-		console.log(JSON.parse(xhttp.responseText));
-		console.log("------------");
+		console.log("------JSON  access_token- user_id-----");
+		
 		user_id=JSON.parse(xhttp.responseText).principal;
+		console.log(user_id);
+		console.log("------------");
 		set_localstorage();
 	}
 	};
