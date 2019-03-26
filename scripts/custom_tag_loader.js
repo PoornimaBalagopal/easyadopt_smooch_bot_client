0181a0283a46d2b1ae98f43cccb26b76b3ac1a15
poornima.balagopal@gmail.com <poornima@14>
2019-03-26 10:39:49 +0530

    Update custom_tag_loader.js

--- scripts/custom_tag_loader.js
+++ scripts/custom_tag_loader.js
@@ -57,7 +57,7 @@ function changeAllTags() {
     const botMessages = chatFrame.querySelectorAll(".msg-wrapper .msg:not(.msg-carousel):not(.right-row)");
     
     botMessages.forEach(msg => {
-        if (!/ea/g.test(msg.innerHTML)) return; // Only edit our customizable elements
+        if (!/ea-/g.test(msg.innerHTML)) return; // Only edit our customizable elements
         msg.innerHTML = unescapeMessage(msg.innerHTML);
     });
 
@@ -73,23 +73,26 @@ function changeLastMessage(messageReceived) {
     const chatFrame = document.getElementById("web-messenger-container").contentDocument;
     const botMessages = chatFrame.querySelectorAll(".msg-wrapper .msg:not(.msg-carousel):not(.right-row)");
     let lastMessage = botMessages[botMessages.length -1];
-    
+  //  alert('lastMessage 0 :'+lastMessage.innerHTML);
    // lastMessage.innerHTML = unescapeMessage(lastMessage.innerHTML);
-  
-	if (/ea/g.test(lastMessage.innerHTML)) lastMessage.innerHTML = unescapeMessage(lastMessage.innerHTML);
+    //alert('/ea-/g.test(lastMessage.innerHTML)  0 :'+/ea-/g.test(lastMessage.innerHTML));
+	
+	 if (/ea-/g.test(lastMessage.innerHTML)) lastMessage.innerHTML = unescapeMessage(lastMessage.innerHTML);
 
     lastMessage.querySelectorAll("ea-video").forEach(ea_video);
     lastMessage.querySelectorAll("ea-footer").forEach(ea_footer);
     lastMessage.querySelectorAll("ea-steps").forEach(ea_steps);
     lastMessage.querySelectorAll("ea-step").forEach(ea_step);
-    lastMessage.querySelectorAll("ea-link").forEach(ea_link);
+    lastMessage.querySelectorAll("ea-link").forEach(ea_link); 
+	
+	 //alert('lastMessage :'+lastMessage.innerHTML);
 }
 
 function deleteTagsInCarouselPreview(messageReceived) {
     const chatFrame = document.getElementById("web-messenger-container").contentDocument;
     const carouselItems = chatFrame.querySelectorAll(".msg-carousel .carousel-item .carousel-description");
 
-    carouselItems.forEach(msg => {
-        msg.innerHTML = deleteTags(msg.innerHTML);
+    carouselItems.forEach(msg => {		
+        msg.innerHTML = deleteTags(msg.innerHTML);		
     });
 }
