28086f1faa9ac5895932999898eec246614a20fa
poornima.balagopal@oracle.com <poornima@14>
2021-09-27 14:19:41 +0530

    demo

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,22 +1,24 @@
 'use strict';
 
 // Set client auth mode - true to enable client auth, false to disable it
-const isClientAuthEnabled = false;
+var isClientAuthEnabled = false;
 
 /**
- * Initializes the SDK and sets a global field with passed name for which
- * it can be referred to later.
+ * Initializes the SDK and sets a global field with passed name for it the can
+ * be referred later
  *
  * @param {string} name Name by which the chat widget should be referred
  */
 function initSdk(name) {
-	 if (!document || !WebSDK) {
+    // Retry initialization later if WebSDK is not available yet
+    if (!document || !WebSDK) {
         setTimeout(function() {
             initSdk(name);
         }, 2000);
         return;
     }
-   if (!name) {
+
+    if (!name) {
         name = 'Bots';          // Set default reference name to 'Bots'
     }
     var Bots;
@@ -28,28 +30,37 @@ function initSdk(name) {
          * In client auth disabled mode, 'channelId' must be passed, 'userId' is optional
          * In client auth enabled mode, 'clientAuthEnabled: true' must be passed
          */
-        let chatWidgetSettings = {
+        var chatWidgetSettings = {
              URI: 'gc3odaejas01-gc35001.botmxp.ocp.oraclecloud.com', // the domain where you want to host your bot
             clientAuthEnabled: isClientAuthEnabled,     // Enables client auth enabled mode of connection if set true
           //  channelId: 'a8a484d4-c319-4420-a4d6-7351d44dae1d',                   // Channel ID, available in channel settings in ODA UI
 		   channelId: '6117d24b-f313-416c-aa7f-58c7672585aa', // here goes the channel id of the web channel you configure, hidden due to security
-		   openChatOnLoad:false,   //Flag to expand chat widget when page is loaded
-		   enableAutocomplete: true, // for autocomplete feature such as you are typing in " i want ..." It will automatically suggest you "i want to order pizza"
-            enableAutocompleteClientCache: true, // Minimizes server calls when user uses autocomplete feature
+            // userId: '<userID>',                      // User ID, optional field to personalize user experience
+            enableAutocomplete: true,                   // Enables autocomplete suggestions on user input
            // enableBotAudioResponse: true,               // Enables audio utterance of skill responses
             enableClearMessage: true,                   // Enables display of button to clear conversation
-			showConnectionStatus: true, // for showing connection status as connected or ready in the chat window
-            enableTimestamp: false,
-			
-			displayActionsAsPills: true, // just a ui feature to show action buttons 
+            enableSpeech: false,                         // Enables voice recognition
+           // speechLocale: WebSDK.SPEECH_LOCALE.EN_US,   // Sets locale used to speak to the skill, the SDK supports EN_US, FR_FR, and ES_ES locales for speech
+            showConnectionStatus: true,                 // Displays current connection status on the header
+			//initBotAudioMuted:true,  //Initializes the skill message utterance in muted mode. This feature can only be activated when you set enableBotAudioResponse to true
+			openChatOnLoad:false,   //Flag to expand chat widget when page is loaded
+			conversationBeginPosition: 'bottom', // for conversation to begin at the bottom
+			//position: { bottom: '20px', right: '20px' },
+            displayActionsAsPills: true, // just a ui feature to show action buttons 
             initUserHiddenMessage: 'Hello', // to intialize the conversation from bots side
+			timestampMode: 'relative',                  // Sets the timestamp mode, relative to current time or default (absolute)
+            enableClearMessage: true,
+			botButtonIcon: 'images/bot-button.png',
+            logoIcon: 'images/bot-white.png',
+            botIcon: 'images/bot-green.png',
+            personIcon: 'images/user-icon.png',
 			font: '12px Verdana, Geneva, sans-serif',
             theme: 'classic',
-			botButtonIcon: 'images/bot-button.png',
-            logoIcon: 'https://easyadopt-bot-client.herokuapp.com/images/bot-white.png',
-            botIcon: 'https://easyadopt-bot-client.herokuapp.com/images/bot-green.png',
-            personIcon: 'https://easyadopt-bot-client.herokuapp.com/images/user-icon.png',
-			 i18n: { // element usefull for english language placeholders and also tool tip
+           // embedded: true,
+            //targetElement: 'chat-container',
+           // embedTopScrollId: 'top-text',
+            //customHeaderElementId: 'custom-header',
+            i18n: { // element usefull for english language placeholders and also tool tip
                 en: {
 
                     audioResponseOff: 'Click to turn audio response on', // Tool tip for the speaker off button
@@ -61,19 +72,30 @@ function initSdk(name) {
                     send: 'Send (Enter)' // Replaces Send tool tip
                 }
             }
+           
         };
 
-       // Initialize SDK
+        // Initialize SDK
         if (isClientAuthEnabled) { // check if authentication is enabled 
             Bots = new WebSDK(chatWidgetSettings, generateToken); // if yes generate a token
         } else {
             Bots = new WebSDK(chatWidgetSettings); // otherwise initialize via widget settings
         }
 
-        // Connect to the ODA
+        // Connect to skill when the widget is expanded for the first time
+        var isFirstConnection = true;
+        Bots.on(WebSDK.EVENT.WIDGET_OPENED, function() {
+            if (isFirstConnection) {
+                Bots.connect();
+                isFirstConnection = false;
+            }
+        });
        
 
         // Create global object to refer Bots
         window[name] = Bots;
     }, 0);
 }
+
+
+
